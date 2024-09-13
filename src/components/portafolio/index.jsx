dd21978cbe30e108b9d7bdbb029a23095da72b6f
Deadcriss86 <112521098+Deadcriss86@users.noreply.github.com>
2024-09-12 18:42:11 -0600

    add json

--- src/components/portafolio/index.jsx
+++ src/components/portafolio/index.jsx
@@ -1,7 +1,15 @@
+import portfolioData from "../../items.json";
 function Portafolio() {
   return (
     <>
-      <div></div>
+      <div className="grid grid-cols-3 gap-4 mx-4">
+        {portfolioData.portfolio.map((item) => (
+          <div key={item.id} className="portfolio-item">
+            <img src={item.image} alt={item.title} className="w-full h-auto" />
+            <h3 className="text-xl font-semibold mt-2">{item.title}</h3>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
