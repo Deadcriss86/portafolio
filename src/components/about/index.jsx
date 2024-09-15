84c2943f014c9ec95bb117cea36084893e37acaa
Deadcriss86 <112521098+Deadcriss86@users.noreply.github.com>
2024-09-15 16:34:50 -0600

    Update index.jsx

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -4,10 +4,10 @@ function About() {
       <div className="text-white space-y-2 px-4 xl:ml-4">
         <h2 className="text-xl sm:text-2xl xl:text-4xl">About me</h2>
         <p className="text-lg sm:text-xl xl:text-2xl">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit iusto
-          tenetur exercitationem, reprehenderit, repellendus, aliquam autem
-          velit quo corrupti nostrum magnam reiciendis. Nulla iste inventore
-          reiciendis a magnam quo doloremque.
+          soy un graduado de la carrera de ingenieria en sistemas
+          computacionales en la universidad valle del grijalva, estudiante de
+          ingles en la escuela de harmon hall y full stack jr salido del
+          bootcamp de kodemia.mx
         </p>
       </div>
     </>
