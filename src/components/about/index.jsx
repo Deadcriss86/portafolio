7c47741af2b69adb2a64141b0b5089175b28224c
Deadcriss86 <112521098+Deadcriss86@users.noreply.github.com>
2024-09-15 12:09:25 -0600

    fix views about and navbar

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -2,8 +2,8 @@ function About() {
   return (
     <>
       <div className="text-white space-y-2 px-4 xl:ml-4">
-        <h2 className="text-4xl">About me</h2>
-        <p className="text-2xl">
+        <h2 className="text-xl sm:text-2xl xl:text-4xl">About me</h2>
+        <p className="text-lg sm:text-xl xl:text-2xl">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit iusto
           tenetur exercitationem, reprehenderit, repellendus, aliquam autem
           velit quo corrupti nostrum magnam reiciendis. Nulla iste inventore
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -4,25 +4,25 @@ function NavBar({ setSelectedComponent }) {
       <div className="navbar bg-base-100 flex justify-end shadow-2xl mb-4">
         <button
           onClick={() => setSelectedComponent("about")}
-          className="btn btn-ghost text-xl hover:text-white"
+          className="btn btn-ghost text-sm sm:text-xl xl:text-2xl hover:text-white"
         >
           About
         </button>
         <button
           onClick={() => setSelectedComponent("resume")}
-          className="btn btn-ghost text-xl hover:text-white"
+          className="btn btn-ghost text-sm sm:text-xl xl:text-2xl hover:text-white"
         >
           Resume
         </button>
         <button
           onClick={() => setSelectedComponent("portafolio")}
-          className="btn btn-ghost text-xl hover:text-white"
+          className="btn btn-ghost text-sm sm:text-xl xl:text-2xl hover:text-white"
         >
           Portafolio
         </button>
         <button
           onClick={() => setSelectedComponent("certificates")}
-          className="btn btn-ghost text-xl hover:text-white"
+          className="btn btn-ghost text-sm sm:text-xl xl:text-2xl hover:text-white"
         >
           certificates
         </button>
